c59bcd0a401d56bccaa5cfb6f9272c68f2e6a818
karlofff24 <120955101+karlofff24@users.noreply.github.com>
2025-09-19 21:48:15 +0900

    изменения

--- src/scenes/gameover.js
+++ src/scenes/gameover.js
@@ -77,7 +77,7 @@ export default function gameover(citySfx) {
 
   // Add the best rank text to the box
   bestRankBox.add([
-    k.text(bestRank, { font: "mania", size: 50 }),
+    k.text(bestRank, { font: "mania", size: 100 }),
     k.anchor("center"), // Center the text within the box
   ]);
 
@@ -93,7 +93,7 @@ export default function gameover(citySfx) {
 
   // Add the current rank text to the box
   currentRankBox.add([
-    k.text(currentRank, { font: "mania", size: 50 }),
+    k.text(currentRank, { font: "mania", size: 100 }),
     k.anchor("center"), // Center the text within the box
   ]);
 
